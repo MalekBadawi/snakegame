0147b4b4437c06dd2a3f7b3f4e959c2aed78af7a
MalekBadawi <92746629+MalekBadawi@users.noreply.github.com>
2021-10-20 12:34:18 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,32 @@
 input.onButtonPressed(Button.A, function () {
     snakehead.turn(Direction.Left, 90)
+    direction += -1
+    if (direction == -1) {
+        direction = 3
+    }
 })
 input.onButtonPressed(Button.AB, function () {
     while (snakehead.get(LedSpriteProperty.X) >= 0 && snakehead.get(LedSpriteProperty.X) <= 5) {
         snakehead.move(1)
         basic.pause(500)
-        if (snakehead.get(LedSpriteProperty.X) == -1 && snakehead.get(LedSpriteProperty.X) <= 6) {
-            game.gameOver()
+        if (body_alive == true) {
+            body.move(1)
         }
     }
 })
 input.onButtonPressed(Button.B, function () {
     snakehead.turn(Direction.Right, 90)
+    direction += 1
+    if (direction == 4) {
+        direction = 0
+    }
+    if (body_alive == true) {
+        body.turn(Direction.Right, 90)
+    }
 })
+let body: game.LedSprite = null
+let body_alive = false
+let direction = 0
 let snakehead: game.LedSprite = null
 snakehead = game.createSprite(0, 0)
 let fruit = game.createSprite(randint(0, 5), randint(0, 5))
@@ -20,5 +34,20 @@ basic.forever(function () {
     if (snakehead.isTouching(fruit)) {
         fruit.set(LedSpriteProperty.X, randint(0, 5))
         fruit.set(LedSpriteProperty.Y, randint(0, 5))
+        if (direction == 0) {
+            body = game.createSprite(snakehead.get(LedSpriteProperty.X) - 1, snakehead.get(LedSpriteProperty.Y))
+            body_alive = true
+        } else if (direction == 1) {
+            body = game.createSprite(snakehead.get(LedSpriteProperty.X), snakehead.get(LedSpriteProperty.Y) - 1)
+            body_alive = true
+        } else if (direction == 2) {
+            body = game.createSprite(snakehead.get(LedSpriteProperty.X) + 1, snakehead.get(LedSpriteProperty.Y))
+            body_alive = true
+        } else if (direction == 3) {
+            body = game.createSprite(snakehead.get(LedSpriteProperty.X), snakehead.get(LedSpriteProperty.Y) + 1)
+            body_alive = true
+        } else {
+        	
+        }
     }
 })
