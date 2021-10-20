f7be593653fe21ac5d856018cf8a5503ad60f1a9
MalekBadawi <92746629+MalekBadawi@users.noreply.github.com>
2021-10-20 11:45:08 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,28 +3,29 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.AB, function () {
     while (snakehead.get(LedSpriteProperty.X) >= 0 && snakehead.get(LedSpriteProperty.X) <= 5) {
-        snakehead.move(1)
-        basic.pause(500)
-        if (snakehead.get(LedSpriteProperty.X) == -1 && snakehead.get(LedSpriteProperty.X) <= 6) {
-            game.gameOver()
-        }
         if (snakehead.isTouching(fruit)) {
-            led.plot(snakehead.get(LedSpriteProperty.X), snakehead.get(LedSpriteProperty.Y))
-            basic.pause(1000)
-            led.unplot(snakehead.get(LedSpriteProperty.X), snakehead.get(LedSpriteProperty.Y))
+            fruit.set(LedSpriteProperty.X, randint(0, 5))
+            fruit.set(LedSpriteProperty.Y, randint(0, 5))
+            bodyx = snakehead.get(LedSpriteProperty.X)
+            bodyy = snakehead.get(LedSpriteProperty.Y)
+            snakehead.move(1)
+            led.plot(bodyx, bodyy)
+            basic.pause(500)
+        } else {
+            snakehead.move(1)
+            basic.pause(500)
         }
     }
 })
 input.onButtonPressed(Button.B, function () {
     snakehead.turn(Direction.Right, 90)
 })
+let bodyy = 0
+let bodyx = 0
 let fruit: game.LedSprite = null
 let snakehead: game.LedSprite = null
 snakehead = game.createSprite(0, 0)
 fruit = game.createSprite(randint(0, 5), randint(0, 5))
 basic.forever(function () {
-    if (snakehead.isTouching(fruit)) {
-        fruit.set(LedSpriteProperty.X, randint(0, 5))
-        fruit.set(LedSpriteProperty.Y, randint(0, 5))
-    }
+	
 })
